52ddab4b9cc43689083280e41a3f2d6fc67878c4
Laurence <laholmes@hotmail.co.uk>
2015-09-13 18:41:15 +0100

    running 538 against betfair, finding kelly betsizes

--- test.py
+++ test.py
@@ -1,35 +1,45 @@
 import nfldb
 import json
 from pprint import pprint
+import betting
 
 db = nfldb.connect()
 q = nfldb.Query(db)
 
-threshold = 0.03
+threshold = 0.04
+bets = []
+bankroll = 100
 
-# filtering
 # returns all games in a gameweek with edge (538 vs betfair price) > threshold
-
 with open("538.json") as data_file:
     data = json.load(data_file)
 
     q.game(season_year=2015, season_type='Regular', week=1)
     for g in q.as_games():
-
         betfairPriceHome = data["weeks"]["1"]["games"][g.gsis_id]["betfairPriceHome"]
         betfairPriceAway = data["weeks"]["1"]["games"][g.gsis_id]["betfairPriceAway"]
         homeTeamWeighting = data["weeks"]["1"]["games"][g.gsis_id]["homeTeamWeighting"]
         awayTeamWeighting = data["weeks"]["1"]["games"][g.gsis_id]["awayTeamWeighting"]
 
-        betfairHomePercentage = 1 / (betfairPriceHome + 1)
-        betfairAwayPercentage = 1 / (betfairPriceAway + 1)
+        betfairHomePercentage = 1.0 / (betfairPriceHome + 1)
+        betfairAwayPercentage = 1.0 / (betfairPriceAway + 1)
 
-        if(betfairHomePercentage >= homeTeamWeighting + threshold):
-            print("found home team match!")
+        if(betfairHomePercentage + threshold <= homeTeamWeighting):
+            bets.append({
+                "team": "home",
+                "gameId": g.gsis_id,
+                "netOdds": betfairPriceHome,
+                "percentageWin": homeTeamWeighting
+            })
 
-        if(betfairAwayPercentage >= awayTeamWeighting + threshold):
-            print("found away team match!")
+        if(betfairAwayPercentage + threshold <= awayTeamWeighting):
+            bets.append({
+                "team": "away",
+                "gameId": g.gsis_id,
+                "netOdds": betfairPriceAway,
+                "percentageWin": awayTeamWeighting
+            })
 
-        print data["weeks"]["1"]["games"][g.gsis_id], g
-
-# constructs list of bets for week with edge
+for bet in bets:
+    bet["size"] = betting.KellySize(bet["netOdds"], bet["percentageWin"], bankroll)
+    print(bet)
