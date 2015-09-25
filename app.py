d165fd18ac1c7147f70ccd1fea125de3efc09889
Laurence <laholmes@hotmail.co.uk>
2015-09-25 18:07:33 +0100

    filling gameweek 3 betfair prices

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 from pprint import pprint
 import betting
 import datetime
+import gameweek
 
 DEBUG = True
 SECRET_KEY = 'hin6bab8ge25*r=x&amp;+5$0kn=-#log$pt^#@vrqjld!^2ci@g*b'
@@ -13,54 +14,14 @@ threshold = 0.04
 bets = []
 bankroll = 100
 
-def calcGameweekBets(week, threshold):
-    q.game(season_year=2015, season_type='Regular', week=week)
-    for g in q.as_games():
-        betfairPriceHome = data["weeks"][week]["games"][g.gsis_id]["betfairPriceHome"]
-        betfairPriceAway = data["weeks"][week]["games"][g.gsis_id]["betfairPriceAway"]
-        homeTeamWeighting = data["weeks"][week]["games"][g.gsis_id]["homeTeamWeighting"]
-        awayTeamWeighting = data["weeks"][week]["games"][g.gsis_id]["awayTeamWeighting"]
-
-        betfairHomePercentage = 1.0 / (betfairPriceHome + 1)
-        betfairAwayPercentage = 1.0 / (betfairPriceAway + 1)
-
-        if(betfairHomePercentage + threshold <= homeTeamWeighting):
-            bets.append({
-                "team": "home",
-                "gameId": g.gsis_id,
-                "netOdds": betfairPriceHome,
-                "percentageWin": homeTeamWeighting
-            })
-
-        if(betfairAwayPercentage + threshold <= awayTeamWeighting):
-            bets.append({
-                "team": "away",
-                "gameId": g.gsis_id,
-                "netOdds": betfairPriceAway,
-                "percentageWin": awayTeamWeighting
-            })
-    return bets
-
-def save_bets(bets):
-    for bet in bets:
-        # segment on week -> bankroll inc
-        print(bet)
-        size = betting.KellySize(bet.netOdds, bet.percentageWin, bankroll)
-
-        Bet.create(
-            game_id = bet["gameId"],
-            amount = size,
-            date = datetime.datetime.now(),
-            odds = bet["netOdds"]
-        )
-
 with open("538.json") as data_file:
     data = json.load(data_file)
 
-    for week in ["1","2"]:
-        bets.append(calcGameweekBets(week, threshold))
+    for week in ["1","2", "3"]:
+        bets.append(gameweek.calc_bets(week, threshold))
 
-    save_bets(bets)
+    print(bets)
+    #save_bets(bets)
 
 # multiple trials for a given edge, assuming mean error = 0
 # for the bets selected, generate random outcome set, 1000 times, show mean winning, std dev
--- gameweek.py
+++ gameweek.py
@@ -8,7 +8,51 @@ import datetime
 db = nfldb.connect()
 q = nfldb.Query(db)
 
-q.game(season_year=2015, season_type='Regular', week=2)
-for g in q.as_games():
-    print(g.gsis_id)
-    print(g)
+# print games for a given gameweek
+def print_bets(year, week):
+    q.game(season_year=year, season_type='Regular', week=week)
+    for g in q.as_games():
+        print(g.gsis_id)
+        print(g)
+
+def calc_bets(week, threshold):
+    bets = []
+    q.game(season_year=2015, season_type='Regular', week=week)
+    for g in q.as_games():
+        betfairPriceHome = data["weeks"][week]["games"][g.gsis_id]["betfairPriceHome"]
+        betfairPriceAway = data["weeks"][week]["games"][g.gsis_id]["betfairPriceAway"]
+        homeTeamWeighting = data["weeks"][week]["games"][g.gsis_id]["homeTeamWeighting"]
+        awayTeamWeighting = data["weeks"][week]["games"][g.gsis_id]["awayTeamWeighting"]
+
+        betfairHomePercentage = 1.0 / (betfairPriceHome + 1)
+        betfairAwayPercentage = 1.0 / (betfairPriceAway + 1)
+
+        if(betfairHomePercentage + threshold <= homeTeamWeighting):
+            bets.append({
+                "team": "home",
+                "gameId": g.gsis_id,
+                "netOdds": betfairPriceHome,
+                "percentageWin": homeTeamWeighting
+            })
+
+        if(betfairAwayPercentage + threshold <= awayTeamWeighting):
+            bets.append({
+                "team": "away",
+                "gameId": g.gsis_id,
+                "netOdds": betfairPriceAway,
+                "percentageWin": awayTeamWeighting
+            })
+    return bets
+
+def save_bets(bets):
+    for bet in bets:
+        # segment on week -> bankroll inc
+        print(bet)
+        size = betting.KellySize(bet.netOdds, bet.percentageWin, bankroll)
+
+        Bet.create(
+            game_id = bet["gameId"],
+            amount = size,
+            date = datetime.datetime.now(),
+            odds = bet["netOdds"]
+        )
